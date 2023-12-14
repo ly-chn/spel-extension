27012f3031871e9977dcb783e6a8006eb7f66540
ly-chn <3293232930@qq.com>
2023-12-14 11:44:59 +0800

    gpg signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,25 @@ import java.net.URI
 
 plugins {
     id("java")
+    id("signing")
     id("maven-publish")
 }
 
 group = "kim.nzxy"
 version = "1.0.0"
 
+dependencies {
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 repositories {
     mavenCentral()
 }
 
-dependencies {
-}
 
 publishing {
     publications {
@@ -23,12 +30,13 @@ publishing {
             version = version
             from(components["java"])
             pom {
+                name = rootProject.name
                 description = "SpEL extension for custom annotation"
                 url = "https://github.com/ly-chn/spel-extension"
                 scm {
-                    url="https://github.com/ly-chn/spel-extension"
-                    connection="scm:git:git://github.com/ly-chn/spel-extension"
-                    developerConnection="scm:git:ssh://github.com:ly-chn/spel-extension"
+                    url = "https://github.com/ly-chn/spel-extension"
+                    connection = "scm:git:git://github.com/ly-chn/spel-extension"
+                    developerConnection = "scm:git:ssh://github.com:ly-chn/spel-extension"
                 }
                 licenses {
                     license {
@@ -39,9 +47,9 @@ publishing {
                 }
                 developers {
                     developer {
-                        name="ly-chn"
-                        email="lyun.chn@gmail.com"
-                        url="https://nzxy.kim"
+                        name = "ly-chn"
+                        email = "lyun.chn@gmail.com"
+                        url = "https://nzxy.kim"
                     }
                 }
             }
@@ -57,4 +65,11 @@ publishing {
             }
         }
     }
+}
+
+signing {
+    val signingKey: String ? by project
+    val signingPassword: String ? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["maven"])
 }
